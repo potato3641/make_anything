221d4f17994b1daed6a1eb187a7e59a8f20168bf
potato3641 <gearg1456@gmail.com>
2024-05-22 09:39:02 +0900

    [DEV] exception handler

--- t2w/main.py
+++ t2w/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, File, HTTPException, Request
+from fastapi import FastAPI, UploadFile, File, HTTPException, Request, Response
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
@@ -6,6 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import os
 
 app = FastAPI(docs_url="/documentation", redoc_url=None)
+
 templates = Jinja2Templates(directory="templates")
 
 app.add_middleware(
@@ -18,6 +19,10 @@ app.add_middleware(
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+class UnicodeEecodeErrorHTTPException(HTTPException):
+    def __init__(self, detail: str = "Unable to encode content"):
+        super().__init__(status_code=400, detail=detail)
+
 @app.get("/")
 async def home(request: Request):
     return templates.TemplateResponse("index.html",{"request":request})
@@ -27,8 +32,18 @@ async def upload_file(request: Request, file: UploadFile = File(...)):
     if file.content_type != 'text/plain':
         raise HTTPException(status_code=400, detail="Invalid file type. Only plain text files are allowed.")
     content = await file.read()
+    if not content:
+        raise HTTPException(status_code=400, detail="File is empty")
     response = RedirectResponse(url="/result/", status_code=307)
-    response.set_cookie(key="content", value=content.decode('utf-8')) # 여기에서 decode를 하지 않으면 str로 넘어가버렷
+    await set_content(response, content)
+    return response
+
+@app.post("/result/", response_class=HTMLResponse)
+async def result(request: Request):
+    content = request.cookies.get("content", "No content available")
+    return templates.TemplateResponse("presentation.html", {"request": request, "content": content})
+
+async def set_content(response: Response, content: bytes) -> None:
     # 작성일 : 24.05.21
     # 작성자 : potato3641
     # 내용 : 쿠키 사용에 대한 주석
@@ -37,12 +52,11 @@ async def upload_file(request: Request, file: UploadFile = File(...)):
     # url은 지저분해보여서 ㅎ;
     # hedaers, body는 fastapi의 middleware사용으로 인해 request가 조정되어 유실되었고
     # python 변수는 여러 요청에 의해 유실될 우려가 있어 사용을 할 수 없었음
-    return response
-
-@app.post("/result/", response_class=HTMLResponse)
-async def result(request: Request):
-    content = request.cookies.get("content")
-    return templates.TemplateResponse("presentation.html", {"request": request, "content": content})
+    try:
+        decoded_content = content.decode('utf-8')
+        response.set_cookie(key="content", value=decoded_content)
+    except UnicodeEncodeError as e:
+        raise UnicodeEecodeErrorHTTPException(detail=str(e))
 
 if __name__ == "__main__":
     import uvicorn

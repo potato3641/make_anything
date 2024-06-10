86b3c6517d7014a6a1e26a8d8acb51490a49ff60
potato3641 <gearg1456@gmail.com>
2024-06-10 23:04:42 +0900

    [DEV] cookie to url

--- t2w/main.py
+++ t2w/main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, UploadFile, File, HTTPException, Request, Response, Form
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi import FastAPI, UploadFile, File, HTTPException, Request, Form
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 # import os
+import base64
+import zlib
 
 app = FastAPI(docs_url="/documentation", redoc_url=None)
 
@@ -19,64 +21,69 @@ app.add_middleware(
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-class UnicodeEecodeErrorHTTPException(HTTPException):
-    def __init__(self, detail: str = "Unable to encode content"):
-        super().__init__(status_code=400, detail=detail)
+# 240604 유니코드 오류 해결로 인한 주석처리
+# class UnicodeEecodeErrorHTTPException(HTTPException):
+#     def __init__(self, detail: str = "Unable to encode content"):
+#         super().__init__(status_code=400, detail=detail)
 
 @app.get("/")
 async def home(request: Request):
+    # index 템플릿
     return templates.TemplateResponse("index.html",{"request":request})
 
-@app.get("/tw2file")
-async def home(request: Request):
+@app.get("/t2wfile")
+async def t2wfile(request: Request):
+    # file page 템플릿
     return templates.TemplateResponse("_file_page.html",{"request":request})
 
-@app.get("/tw2text")
-async def home(request: Request):
+@app.get("/t2wtext")
+async def t2wtext(request: Request):
+    # text page 템플릿
     return templates.TemplateResponse("_text_page.html",{"request":request})
 
 @app.post("/uploadFile/")
 async def upload_file(request: Request, file: UploadFile = File(...)):
+    # 텍스트 파일 변환 요청 처리
     if file.content_type != 'text/plain':
-        raise HTTPException(status_code=400, detail="Invalid file type. Only plain text files are allowed.")
+        raise HTTPException(status_code=400, detail="Only text file")
     content = await file.read()
     if not content:
         raise HTTPException(status_code=400, detail="File is empty")
-    response = RedirectResponse(url="/result/", status_code=307)
-    await set_content(response, content)
-    return response
+    return await upload_text(request, content.decode('utf-8'))
 
 @app.post("/uploadText/")
-async def upload_file(request: Request, content: str = Form()):
+async def upload_text(request: Request, content: str = Form()):
+    # 텍스트 변환 요청 처리
     if not content:
         raise HTTPException(status_code=400, detail="text is empty")
-    response = RedirectResponse(url="/result/", status_code=307)
-    await set_content(response, content)
+    encoded_content = url_compress(content)
+    redirect_url = f"/result/{encoded_content}"
+    response = RedirectResponse(url=redirect_url, status_code=307)
     return response
 
-@app.post("/result/", response_class=HTMLResponse)
-async def result(request: Request):
-    content = request.cookies.get("content", "No content available")
-    return templates.TemplateResponse("presentation.html", {"request": request, "content": content})
+@app.get("/result/{encoded_content}")
+async def get_result(request: Request, encoded_content: str):
+    # 변환 페이지 GET 요청 처리
+    try:
+        decoded_content = url_decompress(encoded_content)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Failed to decode content")
+    return templates.TemplateResponse("presentation.html", {"request": request, "content": decoded_content})
 
-async def set_content(response: Response, content: bytes) -> None:
-    # 작성일 : 24.05.21
-    # 작성자 : potato3641
-    # 내용 : 쿠키 사용에 대한 주석
-    #
-    # url, headers, body, python 변수등을 하려고 시도했으나
-    # url은 지저분해보여서 ㅎ;
-    # hedaers, body는 fastapi의 middleware사용으로 인해 request가 조정되어 유실되었고
-    # python 변수는 여러 요청에 의해 유실될 우려가 있어 사용을 할 수 없었음
-    #try:
-        if isinstance(content, str):
-            decoded_content = content
-        else:
-            decoded_content = content.decode('utf-8')
-        response.set_cookie(key="content", value=decoded_content)
-    #except UnicodeEncodeError as e:
-        print('excepted')
-    #    raise UnicodeEecodeErrorHTTPException(detail=str(e))
+@app.post("/result/{encoded_content}")
+async def post_result(request: Request, encoded_content: str):
+    # 변환 페이지 POST 요청 처리
+    return await get_result(request, encoded_content)
+
+def url_compress(text: str) -> str:
+    # 텍스트 압축
+    compressed = zlib.compress(text.encode('utf-8'))
+    return base64.urlsafe_b64encode(compressed).decode('utf-8')
+
+def url_decompress(encoded_text: str) -> str:
+    # 텍스트 압축해제
+    compressed = base64.urlsafe_b64decode(encoded_text)
+    return zlib.decompress(compressed).decode('utf-8')
 
 if __name__ == "__main__":
     import uvicorn

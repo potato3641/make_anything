48823758e5d3c6b0b3c2cd2b4efe97c292d0986c
potato3641 <gearg1456@gmail.com>
2024-06-11 17:44:10 +0900

    [DEV] add time stamp for access log

--- t2w/gunicorn.conf.py
+++ t2w/gunicorn.conf.py
@@ -11,3 +11,5 @@ worker_class = "uvicorn.workers.UvicornWorker"
 accesslog = f"./logs/gunicorn/access_{datetime.now().strftime('%Y-%m-%d_%H')}.log"
 errorlog = f"./logs/gunicorn/error_{datetime.now().strftime('%Y-%m-%d_%H')}.log"
 loglevel = "info"
+
+access_log_format = '%(t)s %(h)s %(l)s %(u)s %(r)s %(s)s %(b)s "%(f)s" "%(a)s"'

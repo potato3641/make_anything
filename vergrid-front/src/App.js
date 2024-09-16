32d717e887f2bb42463cdd136e40e6432586c874
potato3641 <gearg1456@gmail.com>
2024-09-16 23:47:35 +0900

    [DEV] sheetPage(1/?) saved

--- vergrid-front/src/App.js
+++ vergrid-front/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Base from './layouts/Base';
+import Examples from './components/Examples';
+import SheetPage from './pages/SheetPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Base>
+        <Routes>
+          <Route path="/" element={<Navigate to="/examples" replace />} />
+          <Route path="/examples" element={<Examples />} />
+          <Route path="/sheets" element={<SheetPage />} />
+        </Routes>
+      </Base>
+    </Router>
   );
 }
 

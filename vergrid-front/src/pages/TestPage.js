4616c458e32a24cf5b14827a79ca584ea4807050
potato3641 <gearg1456@gmail.com>
2024-10-30 06:11:43 +0900

    [DEV] fix bug and fixed sheet function

--- vergrid-front/src/pages/TestPage.js
+++ vergrid-front/src/pages/TestPage.js
@@ -3,11 +3,13 @@ import './TestPage.css';
 
 const TestPage = ({ children }) => {
   useEffect(() => {
-    const target = "IF($1$2,1,0)";
-    const regex = /IF\(([^,]+),([^,]+),([^)]+)\)/;
-    const isd = target.match(regex);
-
-    console.log(isd);
+    // test구간
+    const regex = /(?:[\w\u3131-\uD79D]+\([^\)]*\)|\([^\)]*\)|\$\d+\$\d+|[\w\u3131-\uD79D]+)\s*(?:==|<|<=|>|>=|!==)\s*(?:[\w\u3131-\uD79D]+\([^\)]*\)|\([^\)]*\)|\$\d+\$\d+|[\w\u3131-\uD79D]+)/g;
+    const target = '안녕==안녕'
+    const inside = target.match(regex);
+    console.log(target);
+    console.log(inside);
+    // test구간
   }, [])
   return (
     <main>

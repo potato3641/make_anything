e8a8b51baca01f921f6b79fe4681c31252d054cc
potato3641 <gearg1456@gmail.com>
2024-11-05 21:33:10 +0900

    [DEV] fixed calfunction problem & add pre-calculator

--- vergrid-front/src/pages/TestPage.js
+++ vergrid-front/src/pages/TestPage.js
@@ -4,11 +4,6 @@ import './TestPage.css';
 const TestPage = ({ children }) => {
   useEffect(() => {
     // test구간
-    const regex = /(?:[\w\u3131-\uD79D]+\([^\)]*\)|\([^\)]*\)|\$\d+\$\d+|[\w\u3131-\uD79D]+)\s*(?:==|<|<=|>|>=|!==)\s*(?:[\w\u3131-\uD79D]+\([^\)]*\)|\([^\)]*\)|\$\d+\$\d+|[\w\u3131-\uD79D]+)/g;
-    const target = '안녕==안녕'
-    const inside = target.match(regex);
-    console.log(target);
-    console.log(inside);
     // test구간
   }, [])
   return (
